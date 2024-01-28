a3749b10e6fef6440be7a09779a4d5c76a653cee
felipe <profissionalf.andre@gmail.com>
2024-01-28 14:27:01 -0300

    feat: add adding fake data in recipe.html

--- recipes/views.py
+++ recipes/views.py
@@ -1,11 +1,16 @@
 from django.http import HttpRequest
 from django.shortcuts import render
-# Create your views here.
+
+from utils.recipes.factory import make_recipe
 
 
 def home(request: HttpRequest):
-    return render(request, 'recipes/pages/home.html')
+    return render(request, 'recipes/pages/home.html', context={
+        'recipes': [make_recipe for _ in range(10)]
+    })
 
 
 def recipe(request: HttpRequest, id: int):
-    return render(request, 'recipes/pages/recipe-view.html')
+    return render(request, 'recipes/pages/recipe-view.html', context={
+        'recipe': make_recipe()
+    })
