09e4b7ea468ca90f0c6b28fab78eb25ce9cb5ba6
felipe <profissionalf.andre@gmail.com>
2024-05-08 15:58:18 -0300

    style: style form create user

--- authors/views.py
+++ authors/views.py
@@ -1,5 +1,23 @@
 from django.http import HttpRequest
-from django.shortcuts import render
+from django.shortcuts import redirect, render
+from django.http import Http404
+from authors.forms import RegisterForm
 
 def register_view(request: HttpRequest):
-    return render(request, 'authors/pages/register_view.html')
+    register_form_data = request.session.get('register_form_data', None)
+    form = RegisterForm(register_form_data)
+
+    return render(request, 'authors/pages/register_view.html', {
+        'form': form
+    })
+
+def register_create(request: HttpRequest):
+    POST = request.POST
+
+    if not POST:
+        raise Http404('Invalid request')
+    
+    request.session['register_form_data'] = POST
+    form = RegisterForm(POST)
+
+    return redirect('authors:register')
