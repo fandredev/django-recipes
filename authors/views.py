f4577f76ed614c2886995bdcd8359f5a3437c3c2
felipe <profissionalf.andre@gmail.com>
2024-05-10 11:12:20 -0300

    feat: add recipes in dashboard view

--- authors/views.py
+++ authors/views.py
@@ -6,6 +6,7 @@ from django.contrib import messages
 from django.urls import reverse
 from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.decorators import login_required
+from recipes.models import Recipe
 
 
 def register_view(request: HttpRequest):
@@ -93,4 +94,6 @@ def logout_view(request: HttpRequest):
 
 @login_required(login_url="authors:login", redirect_field_name="next")
 def dashboard_view(request: HttpRequest):
-    return render(request, "authors/pages/dashboard.html")
+    recipes = Recipe.objects.filter(is_published=False, author=request.user)
+
+    return render(request, "authors/pages/dashboard.html", {"recipes": recipes})
--- recipes/admin.py
+++ recipes/admin.py
@@ -11,4 +11,27 @@ admin.site.register(Category, CategoryAdmin)
 
 
 @admin.register(Recipe)
-class RecipesAdmin(admin.ModelAdmin): ...
+class RecipesAdmin(admin.ModelAdmin):
+    # Campos que serão exibidos na listagem
+    list_display = ("id", "title", "category", "author", "created_at", "is_published")
+
+    # Campos que serão links para a edição
+    list_display_links = ("title", "category")
+
+    # Campos que serão usados para filtrar os registros (Criará um campo de busca)
+    search_fields = ("title", "description", "slug")
+
+    # Campos que serão usados para filtrar os registros (Criará um campo de filtro)
+    list_filter = ("category", "author", "is_published", "preparation_step_is_html")
+
+    # Quantidade de registros por página
+    list_per_page = 10
+
+    # Campos que poderão ser editados diretamente na listagem
+    list_editable = ("is_published",)
+
+    # Campos que serão ordenados
+    ordering = ("-id",)
+
+    # O campo slug será preenchido automaticamente com o valor do campo title
+    prepopulated_fields = {"slug": ("title",)}
