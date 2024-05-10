2ece67cbbe93cbfbc6bf8a0cadd5b661aad58793
felipe <profissionalf.andre@gmail.com>
2024-05-10 18:49:51 -0300

    feat: add validation recipe_form and create new recipe, edit new recipe

--- authors/views.py
+++ authors/views.py
@@ -101,6 +101,31 @@ def dashboard_view(request: HttpRequest):
     return render(request, "authors/pages/dashboard.html", {"recipes": recipes})
 
 
+def dashboard_recipe_new(request):
+    form = AuthorRecipeForm(
+        data=request.POST or None,
+        files=request.FILES or None,
+    )
+
+    if form.is_valid():
+        recipe: Recipe = form.save(commit=False)
+
+        recipe.author = request.user
+        recipe.preparation_step_is_html = False
+        recipe.is_published = False
+
+        recipe.save()
+
+        messages.success(request, "Salvo com sucesso!")
+        return redirect(reverse("authors:dashboard_recipe_edit", args=(recipe.pk,)))
+
+    return render(
+        request,
+        "authors/pages/dashboard_recipe.html",
+        context={"form": form, "form_action": reverse("authors:dashboard_recipe_new")},
+    )
+
+
 @login_required(login_url="authors:login", redirect_field_name="next")
 def dashboard_recipe_edit(request, id):
     recipe = Recipe.objects.filter(
@@ -112,7 +137,21 @@ def dashboard_recipe_edit(request, id):
     if not recipe:
         raise Http404()
 
-    form = AuthorRecipeForm(data=request.POST or None, instance=recipe)
+    form = AuthorRecipeForm(
+        data=request.POST or None, instance=recipe, files=request.FILES or None
+    )
+
+    if form.is_valid():
+        recipe = form.save(commit=False)
+        recipe.author = request.user
+        recipe.preparation_step_is_html = False
+        recipe.is_published = False
+
+        recipe.save()
+
+        messages.success(request, "Recipe updated successfully")
+
+        return redirect(reverse("authors:dashboard_recipe_edit", args=(id,)))
 
     return render(
         request, "authors/pages/dashboard_recipe.html", context={"form": form}
