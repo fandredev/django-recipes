64fb964a8478cc02c9d329d6dd1c4ec32d5c9dd3
felipe <profissionalf.andre@gmail.com>
2024-05-14 17:37:04 -0300

    feat: add signals to create profile when created user

---
+++ authors/signals.py
@@ -0,0 +1,19 @@
+from django.contrib.auth import get_user_model
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+
+from authors.models import Profile
+
+User = get_user_model()
+
+
+# isso é um sinal que é acionado sempre que um novo usuário é criado
+# Aqui no caso, SEMPRE que um novo usuário é criado, um perfil é criado para ele
+
+
+@receiver(post_save, sender=User)
+def create_profile(sender, instance, created, *args, **kwargs):
+    print("Signal Triggered", instance.username)
+    if created:
+        profile = Profile.objects.create(author=instance)
+        profile.save()
--- recipes/views.py
+++ recipes/views.py
@@ -27,7 +27,9 @@ class RecipeListViewBase(ListView):
         query_set = query_set.filter(
             is_published=True,
         )
-        query_set = query_set.select_related("category")  # use this to n+1 queries
+        query_set = query_set.select_related(
+            "category", "author__profile"
+        )  # use this to n+1 queries
         # query_set = query_set.prefetch_related("category")  # use this to n+n queries
         return query_set
 
