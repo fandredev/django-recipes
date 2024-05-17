91945d117cc8e581b1ffad02b9b2a1bdc5c436a9
felipe <profissionalf.andre@gmail.com>
2024-05-17 18:21:05 -0300

    tests: tests v2 api

--- recipes/tests/test_recipe_api.py
+++ recipes/tests/test_recipe_api.py
@@ -6,23 +6,47 @@ from unittest.mock import patch
 from faker import Faker
 
 
-class RecipeAPIV2Test(test.APITestCase, RecipeMixin):
+class RecipeAPIV2Utils(test.APITestCase, RecipeMixin):
+    faker = Faker()
 
-    def setUp(self):
-        self.faker = Faker()
+    def _get_recipe_raw_data(self):
+        return {
+            "title": "Receita de Bolo de Cenoura",
+            "description": "Receita de bolo de cenoura com cobertura de chocolate",
+            "preparation_time": "30",
+            "preparation_time_unit": "minute",
+            "servings": "10",
+            "servings_unit": "people",
+            "preparation_step": "Passo 1, Passo 2, Passo 3",
+        }
 
-    def __get_recipe_reverse_url(self, reverse_result=None) -> str:
+    def _get_recipe_reverse_url(self, reverse_result=None) -> str:
         api_url = reverse_result or reverse("recipes:recipes-api-list")
 
         return api_url
 
-    def __get_recipe_api_list(self, reverse_result=None):
-        api_url = self.__get_recipe_reverse_url(reverse_result)
+    def _get_recipe_api_list(self, reverse_result=None):
+        api_url = self._get_recipe_reverse_url(reverse_result)
         response = self.client.get(api_url)
         return response
 
+    def _jwt_login_access_token(self):
+        user_data = {
+            "username": self.faker.user_name(),
+            "password": self.faker.password(),
+        }
+        self.make_author(username=user_data["username"], password=user_data["password"])
+
+        response = self.client.post(
+            reverse("recipes:token_obtain_pair"), data={**user_data}
+        )
+        return response.data.get("access")  # type: ignore
+
+
+class RecipeAPIV2Test(RecipeAPIV2Utils):
+
     def test_recipe_api_list_returns_status_code_200(self):
-        response = self.__get_recipe_api_list()
+        response = self._get_recipe_api_list()
 
         self.assertEqual(response.status_code, HTTPStatus.OK)
 
@@ -60,13 +84,23 @@ class RecipeAPIV2Test(test.APITestCase, RecipeMixin):
         api_url = (
             reverse("recipes:recipes-api-list") + f"?category_id={category_wanted.id}"  # type: ignore
         )
-        response = self.__get_recipe_api_list(reverse_result=api_url)
+        response = self._get_recipe_api_list(reverse_result=api_url)
 
         # We should only see recipes from the wanted category
         self.assertEqual(len(response.data.get("results")), 9)  # type: ignore
 
     def test_recipe_api_list_user_must_send_jwt_token_to_create_recipe(self):
-        api_url = self.__get_recipe_reverse_url()
+        api_url = self._get_recipe_reverse_url()
         response = self.client.post(api_url)
 
         self.assertEqual(response.status_code, HTTPStatus.UNAUTHORIZED)
+
+    def test_recipe_api_list_logged_user_can_create_a_recipe(self):
+        data = self._get_recipe_raw_data()
+        response = self.client.post(
+            self._get_recipe_reverse_url(),
+            data=data,
+            HTTP_AUTHORIZATION=f"Bearer {self._jwt_login_access_token()}",
+        )
+
+        self.assertEqual(response.status_code, HTTPStatus.CREATED)
